6c23985afdb204d72d5a3b4f19fdf5b7145c5112
Timmy <generic@mrtim.co>
2024-11-23 22:28:19 +0100

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+    import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+	// let { children } = $props();
+</script>
+
+<!--{@render children()}-->
+
+<div class="w-[350px] mx-auto pt-[350px]">
+    <Tooltip.Provider delayDuration={150}>
+        <Tooltip.Root>
+            <Tooltip.Trigger class="border border-gray-300 p-4 rounded-md">
+                Hover for tooltip
+            </Tooltip.Trigger>
+
+            <Tooltip.Content>
+                <p>Add to library</p>
+            </Tooltip.Content>
+        </Tooltip.Root>
+    </Tooltip.Provider>
+</div>
